5a39084a31ebbad5fd12e5c8b79e4406e1991b58
Gayle Amani <amaniabbie24@gmail.com>
2025-04-26 07:27:43 +0300

    summing elements in a list functionality

---
+++ list-sum.py
@@ -0,0 +1,7 @@
+#create a list
+list = [1,2,4,6,9,0,12]
+#sum_list uses the sum() function to calculate the sum of all
+# values/elements in the list
+sum_list = sum(list)
+#display the final value, which is the sum of all elements
+print(sum_list)
