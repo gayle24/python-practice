af5bf1ae2eb7e4ee759a595bfe521a3b181ab17a
Gayle Amani <amaniabbie24@gmail.com>
2025-04-26 11:27:07 +0300

    factorial using a for loop

--- odd-even-checker.py
+++ odd-even-checker.py
@@ -1,7 +1,7 @@
 #input your value
 x = int(input("Enter your value x: "))
 #using if-else statement to check if number is even or odd
-#if remnant when divided by 2 is 0, then the number is indicated
+#if remnant when divided by 2 is 0, then the number is even
 if x % 2 == 0:
     print("The number is even")
 else:
